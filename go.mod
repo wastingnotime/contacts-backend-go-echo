19054ab729b3905c44ca0e1b29d0761cfb57df9a
Henrique Riccio <henrique.riccio@gmail.com>
2020-06-18 00:27:22 -0300

    minimum working version

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/henriquericcio/contacts-backend-go-echo
+
+go 1.14
+
+require (
+	github.com/google/uuid v1.1.1
+	github.com/labstack/echo/v4 v4.1.16
+	golang.org/x/crypto v0.0.0-20200604202706-70a84ac30bf9 // indirect
+	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
+	golang.org/x/sys v0.0.0-20200615200032-f1bc736245b1 // indirect
+	golang.org/x/text v0.3.3 // indirect
+)
---
+++ main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type contact struct {
+	ID          string `json:"id"`
+	FirstName   string `json:"firstName"`
+	LastName    string `json:"lastName"`
+	PhoneNumber string `json:"phoneNumber"`
+}
+
+func main() {
+	contacts := []contact{
+		{
+			ID:          uuid.New().String(),
+			FirstName:   "Albert",
+			LastName:    "Einstein",
+			PhoneNumber: "1111-1111",
+		}, {
+			ID:          uuid.New().String(),
+			FirstName:   "Mary",
+			LastName:    "Curie",
+			PhoneNumber: "2222-1111",
+		},
+	}
+
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.HideBanner = true
+
+	e.POST("/", func(c echo.Context) error {
+		co := new(contact)
+		if err := c.Bind(co); err != nil {
+			return err
+		}
+
+		co.ID = uuid.New().String()
+
+		contacts = append(contacts, *co)
+
+		return c.NoContent(http.StatusCreated)
+	})
+
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, contacts)
+	})
+
+	e.GET("/:id", func(c echo.Context) error {
+		id := c.Param("id")
+		var co contact
+		for _, ct := range contacts {
+			if ct.ID == id {
+				co = ct
+				break
+			}
+		}
+		if co == (contact{}) {
+			return c.NoContent(http.StatusNotFound)
+		}
+
+		return c.JSON(http.StatusOK, co)
+	})
+
+	e.PUT("/:id", func(c echo.Context) error {
+		id := c.Param("id")
+
+		cp := new(contact)
+		if err := c.Bind(cp); err != nil {
+			return err
+		}
+
+		var co contact
+		for _, ct := range contacts {
+			if ct.ID == id {
+				co = ct
+				break
+			}
+		}
+		if co == (contact{}) {
+			return c.NoContent(http.StatusNotFound)
+		}
+
+		co.FirstName = cp.FirstName
+		co.LastName = cp.LastName
+		co.PhoneNumber = cp.PhoneNumber
+
+		return c.NoContent(http.StatusNoContent)
+	})
+
+	e.DELETE("/:id", func(c echo.Context) error {
+		id := c.Param("id")
+
+		var co contact
+		var ix int = -1
+		for i, ct := range contacts {
+			if ct.ID == id {
+				co = ct
+				ix = i
+				break
+			}
+		}
+		if co == (contact{}) {
+			return c.NoContent(http.StatusNotFound)
+		}
+
+		contacts = remove(contacts, ix)
+
+		return c.NoContent(http.StatusNoContent)
+	})
+
+	e.Logger.Fatal(e.Start(":8010"))
+}
+
+func remove(s []contact, i int) []contact {
+	s[len(s)-1], s[i] = s[i], s[len(s)-1]
+	return s[:len(s)-1]
+}
